6d114115f479f0025f9c3e378c3711740e2e7d21
aaierwagle <badengineer202@gmail.com>
2025-01-15 20:25:44 +0545

    javascripts-revise-2025

---
+++ b.variable.and.constant.js
@@ -0,0 +1,39 @@
+// variable => re writable memory space
+// constant => read only memory space
+
+// variable => we can change value
+// constant => value is fixed
+
+// keyword => reserved word by js
+//  let,const, for, while
+
+// variable declaration
+// let  variableName = "some value"
+
+// let x = 2;
+// x = 100;
+
+// console.log(x);
+
+// const radius = 5;
+// radius = 10;
+
+// console.log(radius);
+
+//variable naming convention
+// variable name cannot be reserved keywords
+// variable name cannot start with number
+// variable name cannot contain special characters @,!,%,&,# except _ and $
+// variable name should be descriptive
+// variable name should be camel case
+
+// upper case  ABC
+// lowercase a b c  nepal
+// camelcase  firstName,lastName, isMarried, isGraduated,hasGotPassMarks
+
+// constant
+// if  constant universal value, then use UPPERCASE
+
+const firstNumber = 5;
+
+const PI = 3.141516;
