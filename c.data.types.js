142306bf65c00459c5db376619e5002613a12c76
aaierwagle <badengineer202@gmail.com>
2025-01-16 22:36:40 +0545

    first commit

---
+++ c.data.types.js
@@ -0,0 +1,40 @@
+//data types
+// string
+// number
+// boolean
+// undefined
+// null
+// object
+// symbol
+// big int
+
+//? string "",'',``
+// let userName = `101`;
+// console.log(userName);
+// console.log(typeof userName);
+
+// ?number
+// let num = Infinity;
+// console.log(num);
+// console.log(typeof num);
+// NaN  => Not a Number
+
+//? boolean
+//  true or false
+
+// let isGraduated = true;
+// console.log(isGraduated);
+// console.log(typeof isGraduated);
+
+//? undefined
+// let x;
+// console.log(x);
+// console.log(typeof x);
+
+// ? null
+// let x = null;
+// console.log(x);
+// it should give null as data type but due to historic bug, it gives "object"
+// console.log(typeof x);
+
+// ? object
---
+++ d.operators.js
@@ -0,0 +1,170 @@
+// operators
+
+// special symbols which performs certain actions
+
+// let x = 5;
+// let y = 6;
+// let sum = x + y; // here "+" is operator and "x"and "y" is operand
+
+// console.log(sum);
+
+// x + y; // expression
+// let result = x + y; //equation
+
+// ? arithmetic operator
+// ? assignment operator
+// ? comparison operator
+// ? logical operator
+
+// ? arithmetic operator
+// ? + , -, *, / , **(power) ,  %(remainder)
+
+// let num1 = 10;
+// let num2 = 2;
+
+// let result = num1 ** num2;
+// console.log(result);
+
+// ? assignment operator
+// ? = , +=, -=, *=, /=,**=,%=
+
+// let x = 5;
+
+// let hasPassedExam = true;
+
+// let a = 5;
+
+// a += 10; // a=a+10
+// console.log(a);
+
+// let b = 7;
+
+// b -= 2; // b = b-2
+
+// console.log(b);
+
+// let sum = 10;
+// sum += 5; //sum = sum +5
+// console.log(sum);
+
+// let c = 25;
+
+// c %= 2; // c=c % 2
+// console.log(c);
+
+// let z = 5;
+// z **= 3; //z = z ** 3
+// console.log(z);
+
+//? Write a JavaScript program to convert temperatures from
+// ? fahrenheit to celsius. Formula:C = (5/9) * (F - 32)
+//? 98.6 deg F = 37 deg C
+
+// let fahrenheit = 102;
+
+// let celsius = (5 / 9) * (fahrenheit - 32);
+
+// console.log(fahrenheit + " deg F =" + celsius + " deg C");
+
+// console.log(`${fahrenheit} deg F = ${celsius} deg C`);
+
+//? Store your current age,name,college/workplace name into  variables.
+//? Output the result like “My name is Nischal and I am 20 years old.
+//? I am an IT student at broadway.”
+
+// let studentName = "Shushant";
+// let age = 21;
+// let college = "Himalayan College";
+
+// console.log(`My name is ${studentName} and I am ${age} years old.
+// I am an engineering student at ${college}.`);
+
+// ?m = m%2,where m=3
+
+// let m = 3;
+// m %= 2; // m = m % 2
+// console.log(m);
+
+//?Create a variable named totalAmount
+//? and store your number as a string.
+// ?Convert it into a number type.
+
+// let totalAmount = "999";
+// totalAmount = Number(totalAmount);
+// console.log(totalAmount);
+// console.log(typeof totalAmount);
+
+// let x = "159";
+// console.log(x, typeof x);
+
+// let y = +x;// + => Number()
+// console.log(y, typeof y);
+
+// let num = "135.5";
+// console.log(Number(num));
+
+// let n = 153;
+// console.log(String(n));
+
+// ?comparison operator
+//  we need at least two items to compare
+// comparison always results in boolean (true/false)
+// ? ==, ===, != ,!==, <, > , <= , >=, ?
+
+// ? == => WEAK comparison, only checks value
+// ? === => STRICT comparison, checks both value and data type
+// let x = 30;
+// let y = "30";
+// console.log(x === y);
+
+// let fruit1 = "apple";
+// let fruit2 = "Apple";
+
+// console.log(fruit1 === fruit2);
+
+// ? != , !==
+
+// let actor1 = "Praddep";
+// let actor2 = "Predeep";
+
+// console.log(actor1 !== actor2);
+
+// let a = true;
+// let b = false;
+// console.log(a !== b);
+
+// <= : less than or equal to
+// console.log(13 >= 13);
+
+//? 3.Create a variable named phoneNumber and store it as a number.
+//? Convert  it into string type.
+
+// let phoneNumber = 4710456;
+// console.log(phoneNumber, typeof phoneNumber);
+
+// let stringPhoneNumber = String(phoneNumber);
+// let stringPhoneNumber = `${phoneNumber}`; //template literal
+// console.log(stringPhoneNumber, typeof stringPhoneNumber);
+
+//?4.Convert string “20ebp” to number and print out the result.
+
+// let randomText = "450";
+
+// let numericValue = +randomText;
+
+// console.log(numericValue, typeof numericValue);
+
+//? 4.Convert string “20ebp” to number and print out the result.
+// const text = "nep10";
+
+// const number = parseInt(text);
+
+// console.log(number, typeof number);
+
+// Check if 5!==10. Explain the answer.
+
+// console.log(5 !== 10);
+
+// console.log("D" < "d");
+
+// console.log("ram" + 1);
