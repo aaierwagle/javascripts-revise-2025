5bfbeb9a96efbbc989a535a69eb30a5618c8e19a
aaierwagle <badengineer202@gmail.com>
2025-01-18 20:57:40 +0545

    message

---
+++ e.operators2.js
@@ -0,0 +1,61 @@
+// ?logical operator
+// ?NOT, AND, OR
+
+// ?NOT (!)
+
+// let isGraduated = false;
+
+// console.log(!isGraduated);
+
+// ? OR (||)
+// ?pipe symbol
+
+// let condition1 = true; //1
+// let condition2 = false; //0
+
+// console.log(condition1 || condition2);
+
+// ? short circuit in or
+//  if any one condition is true, then the result is always true
+
+// console.log(false || true || false || false);
+
+// ? AND (&&)
+// ? ampersand
+
+// console.log(true && true && false && true);
+
+// ? short circuit
+// if any one condition is false, then the result is always false
+
+// let result1 = 2 < 9; //true
+
+// let result2 = "true" == "True"; //false
+
+// console.log(result1 && result2);
+
+// ?Let a=2,b=-3,c=5,d=10,e=9. Calculate
+
+let a = 2;
+let b = -3;
+let c = 5;
+let d = 10;
+let e = 9;
+
+//?1. a>b AND c<=d
+// let condition1 = a > b; //true
+// let condition2 = c <= d; //true
+// console.log(condition1 && condition2);
+
+//? 2.a+b<c OR d*a>=c // true
+
+// let firstCondition = a + b < c; //true
+
+// ?3.(a**c===d AND d===e+a-1)  OR d % e!==1 //false
+
+// let firstCondition = a ** c === d; //false
+// let secondCondition = d === e + a - 1;
+// let thirdCondition = d % e !== 1;
+
+// let isEarthRound = true;
+// console.log(!isEarthRound);
---
+++ f.scopes.js
@@ -0,0 +1,19 @@
+// let userName = "reena1019";
+
+// {
+//   let userName = "sabin111";
+
+//   {
+//     let userName = "rajeev";
+//     console.log(userName);
+//   }
+// }
+
+// let actor = "Tom Hanks";
+// // console.log(actor);
+
+// {
+//   let actor = "Tim Robbins";
+// }
+
+// console.log(actor);
